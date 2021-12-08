ccd7aff840ff20be4a234e22cfe0013ab01f6fb9
Stephen Magill <stephen@muse.dev>
2021-12-07 22:43:41 -0500

    Trigger bugs with Infer, ErrorProne, FindSecBugs, and Semgrep

--- src/main/java/com/sonatype/lift_test/Hello.java
+++ src/main/java/com/sonatype/lift_test/Hello.java
@@ -1,7 +1,19 @@
 package lift_test;
 
 public class Hello {
+    static String[] arr = null;
+
+    public static void foo(String[] x) {
+        System.out.println(x[0]);
+    }
+
     public static void main(String[] args) {
+        foo(arr);
         System.out.println("Hello World");
+        int test_lift = (int) Math.random();
+        String s = String.valueOf(test_lift);
+        System.out.println(s);
+        if(s == arr[0])
+            System.out.println("true");
     }
 }
